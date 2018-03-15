485d77119719086275fe3ea8229eb0884b9ea177
vishnueaswaran <vishnuedamana@gmail.com>
2018-03-15 12:44:08 +0530

    adc and bare metal

---
+++ ARMadc/src/main.c
@@ -0,0 +1,30 @@
+/*
+ * ADC in ARM
+ *
+ * Author : Vishnu Easwaran E 
+ *
+ */
+#include "stm32f30x_conf.h"
+
+int main (void)
+{
+	//ADC calibration
+	ADC1->CR &= ~ADC_CR_ADEN; //0 in ADCEN to diable ADC.
+
+	ADC1->CR 
+
+	ADC1->CR |= ADC_CR_ADCAL; //put a 1 in calibration register
+
+	while ((ADC1->CR & ADC_CR_ADCAL) != 0) {} //waiting till the calibration is over
+
+	//select a clock source
+	RCC->AHBENR |= RCC_AHBENR_ADC12EN;
+
+	//ADc
+
+	while(1)
+	{
+		//start conversion
+	}
+
+}
